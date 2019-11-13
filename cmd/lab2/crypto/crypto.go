7d176d236522f5229fc77983950ead15d495b2e7
kochuv <veronika.kochugova@gmail.ru>
2019-11-13 20:15:39 +0300

    Add package crypt.

---
+++ cmd/lab2/crypto/crypto.go
@@ -0,0 +1,79 @@
+package crypto
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"golang.org/x/crypto/pkcs12"
+)
+
+// SIGN: https://stackoverflow.com/questions/52775864/how-to-read-pkcs12-content-in-golang-i-have-example-in-php
+// Verify: https://medium.com/@raul_11817/golang-cryptography-rsa-asymmetric-algorithm-e91363a2f7b3
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("Fatal error ", err.Error())
+		os.Exit(1)
+	}
+}
+
+func GetCertificate(file string) x509.Certificate {
+	// Load public certificate to buffer
+	buf, err := ioutil.ReadFile(file)
+	checkError(err)
+
+	// Decode certificate from buffer and create block
+	block, _ := pem.Decode([]byte(buf))
+	if block == nil {
+		panic("failed to parse certificate PEM")
+	}
+
+	// Parse block and create x509.Certificate instance
+	cert, err := x509.ParseCertificate(block.Bytes)
+	checkError(err)
+
+	return *cert
+}
+
+func GetKeyStore(file, keystorePassword string) interface{} {
+
+	// Load PKCS12 KeyStore
+	buf, err := ioutil.ReadFile(file)
+	checkError(err)
+
+	// Get KeyStore.
+	keyStore, _, err := pkcs12.Decode(buf, keystorePassword)
+	checkError(err)
+
+	return keyStore
+}
+
+func GetPrivateKeyFromKeyStore(keyStore interface{}) *rsa.PrivateKey {
+	return keyStore.(*rsa.PrivateKey)
+}
+
+func GetPublicKeyFromCert(cert x509.Certificate) *rsa.PublicKey {
+	return cert.PublicKey.(*rsa.PublicKey)
+}
+
+func EncryptData(data string, pubKey *rsa.PublicKey) string {
+	dataBytes := []byte(data)
+	hash := sha256.New()
+	encryptedBytes, err := rsa.EncryptOAEP(hash, rand.Reader, pubKey, dataBytes, nil)
+	checkError(err)
+	return string(encryptedBytes)
+}
+
+func DecryptData(data string, privKey *rsa.PrivateKey) string {
+	dataBytes := []byte(data)
+	hash := sha256.New()
+	decryptedBytes, err := rsa.DecryptOAEP(hash, rand.Reader, privKey, dataBytes, nil)
+	checkError(err)
+	return string(decryptedBytes)
+}
--- cmd/lab2/main.go
+++ cmd/lab2/main.go
@@ -1,68 +1,23 @@
 package main
 
 import (
-	"crypto/x509"
-	"encoding/pem"
 	"fmt"
-	"io/ioutil"
 
-	"golang.org/x/crypto/pkcs12"
+	lib "./crypto"
 )
 
-// public static byte[] encryptData(byte[] data,
-// X509Certificate encryptionCertificate)
-// throws CertificateEncodingException, CMSException, IOException {
-// byte[] encryptedData = null;
-// if (null != data && null != encryptionCertificate) {
-// CMSEnvelopedDataGenerator cmsEnvelopedDataGenerator
-// = new CMSEnvelopedDataGenerator();
-// JceKeyTransRecipientInfoGenerator jceKey
-// = new JceKeyTransRecipientInfoGenerator(encryptionCertificate);
-// cmsEnvelopedDataGenerator.addRecipientInfoGenerator(jceKey);
-// CMSTypedData msg = new CMSProcessableByteArray(data);
-// OutputEncryptor encryptor
-// = new JceCMSContentEncryptorBuilder(CMSAlgorithm.AES128_CBC)
-// .setProvider("BC").build();
-// CMSEnvelopedData cmsEnvelopedData = cmsEnvelopedDataGenerator
-// .generate(msg,encryptor);
-// encryptedData = cmsEnvelopedData.getEncoded();
-// }
-// return encryptedData;
-// }
-
-func encryptData(cer x509.Certificate, keyStore pkcs12.KeyStore) {
-
-}
-
 func main() {
+	cert := lib.GetCertificate("examples/public.cer")
+	keyStore := lib.GetKeyStore("examples/private.p12", "password")
 
-	// Load public certificate
-	buf, err := ioutil.ReadFile("examples/public.cer")
-	fmt.Println(err)
-	fmt.Println(buf)
+	pubKey := lib.GetPublicKeyFromCert(cert)
+	privKey := lib.GetPrivateKeyFromKeyStore(keyStore)
 
-	// Decode certificate fron buffer and create block
-	block, _ := pem.Decode([]byte(buf))
-	if block == nil {
-		panic("failed to parse certificate PEM")
-	}
+	secret := "Secret msg!"
+	encrypted := lib.EncryptData(secret, pubKey)
+	decrypted := lib.DecryptData(encrypted, privKey)
 
-	// Parse block and create x509.Certificate instance
-	cert, err := x509.ParseCertificate(block.Bytes)
-
-	fmt.Println(err)
-	fmt.Println(cert)
-
-	// blockType := "RSA PRIVATE KEY"
-	keyPassword := []byte("password")
-	// keystorePassword := []byte("password")
-
-	// Create Private PKCS12 KeyStore
-	p12, _ := ioutil.ReadFile("examples/private.p12")
-	blocks, err := pkcs12.ToPEM(p12, string(keyPassword))
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(blocks)
-
+	fmt.Println("\nSecret msg: " + secret)
+	fmt.Println("\nEncrypted msg: " + encrypted)
+	fmt.Println("\nDecrypted msg: " + decrypted)
 }
