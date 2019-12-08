18be6e4c89e099258ed18205f88938196d82682a
kochuv <veronika.kochugova@gmail.com>
2019-12-08 22:49:21 +0300

    fix

--- cmd/lab2/main.go
+++ cmd/lab2/main.go
@@ -2,13 +2,30 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	lib "./crypto"
+	"github.com/akamensky/argparse"
 )
 
-// TODO: add argparse
-// TODO: add sign
+func parseArgs() (secret string) {
+	parser := argparse.NewParser("main.go", "Encrypt/Decrypt msg")
+	s := parser.String("s", "secret", &argparse.Options{Required: true, Help: "secret msg"})
+	err := parser.Parse(os.Args)
+	if err != nil {
+		fmt.Print(parser.Usage(err))
+	}
+
+	if *s == "" {
+		*s = "Secret msg"
+	}
+	return *s
+}
+
 func main() {
+
+	secret := parseArgs()
+
 	cert := lib.GetCertificate("examples/public.cer")
 	keyStore := lib.GetKeyStore("examples/private.p12", "password")
 
@@ -16,7 +33,6 @@ func main() {
 	fmt.Println(*pubKey)
 	privKey := lib.GetPrivateKeyFromKeyStore(keyStore)
 
-	secret := "Secret msg!"
 	encrypted := lib.EncryptData(secret, pubKey)
 	decrypted := lib.DecryptData(encrypted, privKey)
 
